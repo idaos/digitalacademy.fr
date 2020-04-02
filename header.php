b44ebf4160d4640fad57d09fc168da583fc347a5
kzlucas <lucas.tesseron@gmail.com>
2020-04-02 15:28:45 +0200

    hot fix mobile - courses page

--- header.php
+++ header.php
@@ -99,7 +99,7 @@ adsblocker: adsblocked
                     <a title="Plus d'info" href="<?php echo the_field('blue_banner_link', 'options') ?>" target="_blank">
                         <?php if ( get_field( 'blue_banner_link', 'options'  ) ): ?>
                         <img src="<?php echo get_template_directory_uri(); ?>/landing-page-catalogue/res/img/info-icon.svg" width="18" alt="info">
-                    <?php endif; ?>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
