7cd06f00e48072cd017655e2e297f18a7ed9aee0
Lucas Tesseron <lucas.tesseron@gmail.com>
2020-04-30 14:38:18 +0200

    Fix contact banner display on mobile

--- tpl/cta-contact.php
+++ tpl/cta-contact.php
@@ -1,3 +1,19 @@
 <div class="full-width bg-orange full-width-contact">
-    <p class="clearfix"><span class="hidden-xs">Pour en savoir plus, appelez-nous au</span> <span class="underline hidden-xs"><a href="tel:0977215321">09 77 21 53 21</a></span><span class="hidden-xs"> OU </span><a href="/contact/" class="btn-white">Contactez-nous</a></p>
+    <p class="clearfix"><span style="margin-bottom:.5em;display: inline-block;">
+        Pour en savoir plus, appelez-nous au
+        </span> 
+        <span>
+            <a href="tel:0977215321" class="noWrap" style="color:#fff;">09 77 21 53 21</a>
+        </span>
+        <span stlye="margin-left: .5em;"> ou </span>
+        <a href="/contact/" class="btn-white">Contactez-nous</a></p>
 </div>
+
+
+<style>
+    @media (max-width: 400px) {
+        .bg-orange .btn-white{
+            display: block;
+        }
+    }
+</style>
