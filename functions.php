9a7023a241639f2d970ee5f696ff9e07f6636c82
kzlucas <lucas.tesseron@gmail.com>
2022-11-13 15:35:18 +0100

    php err

--- functions.php
+++ functions.php
@@ -1617,8 +1617,12 @@ function admin_custom_css() {
 function display_user_roles(){
     $user_id = get_current_user_id();
     $user_info = get_userdata( $user_id );
-    $user_roles = implode(', ', $user_info->roles);
-    return $user_roles;
+    if($user_info){
+        $user_roles = implode(', ', $user_info->roles);
+        return $user_roles;
+    }else{
+        return '';
+    }
 }
 
 function admin_bar(){
